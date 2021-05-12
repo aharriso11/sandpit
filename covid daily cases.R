114209f9f6cb3900855d23b26b4354ecab7db876
aharriso11 <78227435+aharriso11@users.noreply.github.com>
2021-05-12 21:37:29 +0100

    R update

--- covid daily cases.R
+++ covid daily cases.R
@@ -6,7 +6,8 @@ library("zoo")
 
 # IMPORT DATASET ----
 
-covid_cases_csv <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newCasesBySpecimenDate&format=csv")) %>%
+covid_cases_csv <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newCasesBySpecimenDate&format=csv"))
+covid_cases_csv <- covid_cases_csv %>%
   dplyr::mutate(cases_07da = zoo::rollmean(newCasesBySpecimenDate, k = 7, fill = NA))
 
 # FORMAT DATA ----
@@ -17,7 +18,7 @@ less_seven_days <- less_recent_days - 7
 
 # PLOT DATA ----
 
-covid_cases_plot <- ggplot(covid_cases_csv[which(covid_cases_csv$date>"2020-11-01" & covid_cases_csv$date<less_recent_days),], aes(x = date, y = cases_07da)) + 
+covid_cases_plot <- ggplot(covid_cases_csv[which(covid_cases_csv$date>"2020-10-31" & covid_cases_csv$date<less_recent_days),], aes(x = date, y = cases_07da)) + 
   geom_point(shape = 1, colour = "red", size=2) +
   geom_smooth(data=subset(covid_cases_csv, covid_cases_csv$date >= less_seven_days), method = "lm", colour = "black", size=0.5, fullrange=TRUE, se=FALSE) +
   labs(caption = "Data from Public Health England / https://coronavirus.data.gov.uk")
---
+++ covid dorset cases.R
@@ -0,0 +1,48 @@
+# LOAD LIBRARIES ----
+
+library("ggplot2")
+library("dplyr")
+library("zoo")
+library("reshape2")
+
+# IMPORT DATASETS ----
+
+covid_cases_dor <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=ltla&areaCode=E06000059&metric=newCasesBySpecimenDate&format=csv"))
+covid_cases_bcp <- read.csv(url("https://api.coronavirus.data.gov.uk/v2/data?areaType=ltla&areaCode=E06000058&metric=newCasesBySpecimenDate&format=csv"))
+
+# MUNGE DATA ----
+
+# apply a seven day rolling average to each table
+covid_cases_dor <- covid_cases_dor %>%
+  dplyr::mutate(cases_07da_dor = zoo::rollmean(covid_cases_dor$newCasesBySpecimenDate, k = 7, fill = NA))
+
+covid_cases_bcp <- covid_cases_bcp %>%
+  dplyr::mutate(cases_07da_bcp = zoo::rollmean(covid_cases_bcp$newCasesBySpecimenDate, k = 7, fill = NA))
+
+# merge bcp and dorset tables into one
+covid_cases_com <- merge(covid_cases_bcp, covid_cases_dor, by.x = "date", by.y = "date")
+
+# remove old data we don't want
+covid_cases_com <- subset(covid_cases_com, date > "2020-10-31", select = c("date", "cases_07da_bcp", "cases_07da_dor"))
+
+# convert wide data into long
+melt.cases <- melt(covid_cases_com, id=c("date"), variable.name = "area", value.name = "cases")
+
+# define the date format
+melt.cases$date = as.Date(melt.cases$date, "%Y-%m-%d")
+
+# PLOT DATA ----
+
+# create plot and geom
+covid_cases_plot <- ggplot(melt.cases, aes(x = date, y = cases, col = area)) +
+  geom_point(shape = 1, size = 2) + scale_colour_manual(name = "Local authority", values = c("cases_07da_dor" = "green4", "cases_07da_bcp" = "darkmagenta"), labels = c("BCP", "Dorset")) +
+  labs(caption = "Data from Public Health England / https://coronavirus.data.gov.uk")
+
+# set plot params
+covid_cases_plot + 
+  scale_y_continuous(trans = 'log10', breaks = c(5,10,20,50,100,200,500)) +
+  scale_x_date(date_labels = "%B %Y", date_breaks = "1 month") +
+  ggtitle("Dorset covid cases - 7 day average by specimen date (log scale)") +
+  xlab("Date") +
+  ylab("Cases") +
+  theme_bw()
