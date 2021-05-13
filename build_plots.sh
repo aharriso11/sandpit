2db9949c5cea235a5c1676e61c293df382ed2be5
aharriso11 <78227435+aharriso11@users.noreply.github.com>
2021-05-13 22:01:03 +0100

    'push_2021_05_13_2200'

--- build_plots.sh
+++ build_plots.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -x #echo on
 
+rm -f timestamp*
+
 commit_string=$(date '+%Y_%m_%d_%H%M')
+
+touch "timestamp${commit_string}"
 
 rscript "/users/andrewharrison/Documents/R/covid admissions dorset.R"
 rscript "/users/andrewharrison/Documents/R/covid dorset cases.R"
