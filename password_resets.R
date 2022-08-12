eaf18dfd64b103e38ec94c005133340cd45f6a96
Andrew Harrison <78227435+aharriso11@users.noreply.github.com>
2022-08-12 17:40:57 +0100

    update

--- password_resets.R
+++ password_resets.R
@@ -29,11 +29,15 @@ df <- read.csv(file = "resets.csv")
 
 # TRANSFORM DATA ----
 
+# remove computer account password resets
+df <- df %>%
+  filter(src_user_name != "SVC_JOIN")
+
 # the date/time stamp from Splunk is very detailed, it's simpler to split this using "T" as a demlimiter
 df2 <- cSplit(df, "X_time", "T")
 
 # Now we have X_time_1 and X_time_2. We don't need _2 so we'll subset the columns to get rid of it.
-df2 <- subset(df2, select = c("src_user","X_time_1"))
+df2 <- subset(df2, select = c("Target_User_Name","src_user_name","EventCode","X_time_1"))
 
 # X_time_1 is text so we need to specify it as a date
 df2$X_time_1 <- as.Date(df2$X_time_1, "%Y-%m-%d")
@@ -41,6 +45,7 @@ df2$X_time_1 <- as.Date(df2$X_time_1, "%Y-%m-%d")
 # create a new dataframe and use a dplyr pipeline to
 # count the number of occurrences of a date
 df3 <- df2 %>%
+  group_by(EventCode) %>%
   count(X_time_1)
 
 # because we want to group by day of the week, use wday to add this as a new column
@@ -55,12 +60,13 @@ df_plot <- ggplot() +
   # create a column chart using df2 with our new Day column defining how the objects get filled
   geom_col(data = df3, aes(x = X_time_1, y = n, fill = Day)) +
   # set the x axis labels
-  scale_x_date(date_labels = "%a %d %b %y", date_breaks = "1 day") +
+  scale_x_date(date_labels = "%a %d %b %y", date_breaks = "1 week") +
   # set axis titles
-  xlab("Expiry date") +
-  ylab("Number of password resets") +
+  xlab("Date") +
+  ylab("Number") +
   # give the plot a title
-  ggtitle("Domain actual password resets") +
+  ggtitle("Domain password changes and resets") +
+  facet_grid( ~ EventCode) +
   # set a theme - I usually use the base theme
   theme_base() +
   # customise the theme
